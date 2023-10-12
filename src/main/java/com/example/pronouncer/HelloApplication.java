dc85f3ca5269676aec190962dd881dd67c53656d
Anton Butkevich <anton.butkevich@gmail.com>
2023-10-12 20:15:23 +0300

    Adding JNativeHook dependency

--- src/main/java/com/example/pronouncer/HelloApplication.java
+++ src/main/java/com/example/pronouncer/HelloApplication.java
@@ -1,11 +1,16 @@
 package com.example.pronouncer;
 
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +20,19 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            throw new RuntimeException(e);
+        }
+
+        GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
+            @Override
+            public void nativeKeyPressed(NativeKeyEvent nativeEvent) {
+                System.out.println(nativeEvent);
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.pronouncer {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires com.github.kwhat.jnativehook;
 
     opens com.example.pronouncer to javafx.fxml;
     exports com.example.pronouncer;
